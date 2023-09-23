73113dd2667c9d66ebda2653c706b245c22aa211
Gavutham <kgavutham2005@gmail.com>
2023-09-23 09:45:25 +0530

    create demo homepage for dl integration

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
 import "./Home.scss";
+import axios from "axios";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [message, setMessage] = useState("");
+  const [file, setFile] = useState(null);
+
+  const handleSubmit = () => {
+    setMessage("Processing the request...");
+    console.log(file);
+    const formData = new FormData();
+    formData.append("image", file);
+
+    axios
+      .post("http://127.0.0.1:5000/predict", formData)
+      .then((response) => {
+        setMessage(response.data["prediction"]);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleImageChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  return (
+    <div className="home">
+      <div className="content">
+        <h1>Welcome!</h1>
+        <p>Please select an image:</p>
+        <input
+          id="image"
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+        />
+        <label htmlFor="image">Upload Image</label>
+        <button onClick={handleSubmit}>Submit</button>
+        {file && <img src={URL.createObjectURL(file)} />}
+        {message && <p className="message">{message}</p>}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
